b1610a0f15f5a9756f967a762266d8b431e7bf4d
guybarreto <guybarreto@icloud.com>
2024-03-01 22:51:29 -0300

    Requirement 3

---
+++ src/main/java/com/betrybe/agrix/controllers/AuthController.java
@@ -0,0 +1,40 @@
+package com.betrybe.agrix.controllers;
+
+import com.betrybe.agrix.controllers.dto.AuthDto;
+import com.betrybe.agrix.controllers.dto.TokenDto;
+import com.betrybe.agrix.services.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+  private final AuthenticationManager authenticationManager;
+  private final TokenService tokenService;
+
+  @Autowired
+  public AuthController(AuthenticationManager authenticationManager,
+      TokenService tokenService) {
+    this.authenticationManager = authenticationManager;
+    this.tokenService = tokenService;
+  }
+
+  @PostMapping("/login")
+  public TokenDto login(@RequestBody AuthDto authDto) {
+    UsernamePasswordAuthenticationToken usernamePassword =
+        new UsernamePasswordAuthenticationToken(authDto.username(), authDto.password());
+
+    Authentication auth = authenticationManager.authenticate(usernamePassword);
+
+    String token = tokenService.generateToken(auth.getName());
+
+    return new TokenDto(token);
+  }
+}
--- src/main/java/com/betrybe/agrix/models/entities/Person.java
+++ src/main/java/com/betrybe/agrix/models/entities/Person.java
@@ -7,13 +7,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.util.Objects;
+import jakarta.persistence.Table;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 /**
  * Class representing a person.
  */
 @Entity
-public class Person {
+@Table(name = "persons")
+public class Person implements UserDetails, GrantedAuthority {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,6 +30,13 @@ public class Person {
   private String password;
 
   private Role role;
+
+  public Person(Long id, String username, String password, Role role) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.role = role;
+  }
 
   public Person() {
   }
@@ -62,17 +74,33 @@ public class Person {
   }
 
   @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Person person = (Person) o;
-    return Objects.equals(id, person.id) && Objects.equals(username,
-        person.username) && Objects.equals(password, person.password)
-        && Objects.equals(role, person.role);
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(this);
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
+  @Override
+  public String getAuthority() {
+    return this.getRole().getName();
   }
 }
 
